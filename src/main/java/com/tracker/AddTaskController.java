b7362c67a300b30133e773e9988783b7cdbbde90
Eric <eric@gtfo.org>
2026-04-07 00:15:14 -0400

    AddTaskController in progress

--- src/main/java/com/tracker/AddTaskController.java
+++ src/main/java/com/tracker/AddTaskController.java
@@ -1,6 +1,10 @@
 package com.tracker;
 
+import java.awt.TextArea;
+import java.awt.TextField;
 import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.MenuButton;
 
 /**
  * Initializes the Add Task scene.  Populates the priority dropdown menu and
@@ -11,7 +15,29 @@ import javafx.fxml.FXML;
  */
 public class AddTaskController {
 
+  @FXML private TextField txtField_taskTitle;
+  @FXML private DatePicker datePicker_dueDate;
+  @FXML private MenuButton dropdown_priority;
+  @FXML private MenuButton dropdown_taskType;
+  @FXML private TextArea textArea_description;
+
   // TODO: get list of task types and populate task type dropdown
+  // who is doing task types?
+
+  // TODO: populate priority dropdown
+
+
+  public void addTaskAction() {
+    // TODO: if title, date, priority, or task type are blank, inform user
+    if (txtField_taskTitle.getText().isEmpty() ||
+        datePicker_dueDate.getValue().toString().isEmpty() ||
+        dropdown_priority.getText().isEmpty() ||
+        dropdown_taskType.getText().isEmpty()
+    ) {
+      return;
+    }
+
+  }
 
   public void cancelAction() {
     SceneManager.showTaskList();
