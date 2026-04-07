fbb91e43f7501b470b84e01511eb87ff2efbcd97
darenteria-cpu <darenteria@csumb.edu>
2026-04-06 20:40:03 -0700

    Completed admin_dashboard.fxml. Next create class AdminDashboardController.java.

---
+++ src/main/java/com/tracker/AdminDashboardController.java
@@ -0,0 +1,20 @@
+
+package com.tracker;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+/**
+ * Represents the admin dashboard controller, which manages the admin dashboard view
+ * and its interactions.
+ * The admin dashboard provides an overview of task statistics
+ * and allows the admin to navigate to other sections of the application,
+ * such as admin settings.
+ *
+ * @author David Renteria
+ * @version 0.1.0
+ * @since 4/6/2026
+ */
+public class AdminDashboardController {
+
+}
