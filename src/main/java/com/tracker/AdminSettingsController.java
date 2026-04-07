0fbda2cf74f870197e92d881c1255a4cdfe98e2e
darenteria-cpu <darenteria@csumb.edu>
2026-04-07 16:59:06 -0700

    created AdminSettingsController.java

---
+++ src/main/java/com/tracker/AdminSettingsController.java
@@ -0,0 +1,11 @@
+package com.tracker;
+
+/**
+ * Brief description of what this class does
+ * 
+ * @author David Renteria
+ * @since 4/7/2026
+ * @version 0.1.0
+ */
+    public class AdminSettingsController {
+}
