994bd6ccdce03210d6a660b129dd3f285b3ee385
darenteria-cpu <darenteria@csumb.edu>
2026-04-07 17:21:11 -0700

    built TaskType.java

--- src/main/java/com/tracker/TaskType.java
+++ src/main/java/com/tracker/TaskType.java
@@ -4,8 +4,24 @@ package com.tracker;
  * Brief description of what this class does
  *
  * @author David Renteria
- * @version 0.1.0
+ * @version 0.2.0
  * @since 4/7/2026
  */
+
 public class TaskType {
+    private final int id;
+    private final String name;
+
+    public TaskType(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
