70572469b9fd17183ca30156845cfb651a4da0a9
darenteria-cpu <darenteria@csumb.edu>
2026-04-08 13:09:06 -0700

    created SettingsDAO.java and SQLiteSettingsDAO.java.

---
+++ src/main/java/com/tracker/dao/SQLiteSettingsDAO.java
@@ -0,0 +1,12 @@
+package com.tracker.dao;
+
+/**
+ * SQLite implementation of the SettingsDAO interface.
+ * Handles all database interactions related to app settings.
+ *
+ * @author David Renteria
+ * @version 0.1.0
+ * @since 4/8/2026
+ */
+public class SQLiteSettingsDAO {
+}
---
+++ src/main/java/com/tracker/dao/SettingsDAO.java
@@ -0,0 +1,13 @@
+package com.tracker.dao;
+
+/**
+ * Represents database operations for application settings
+ * This interface defines methods for retrieving and updating application settings,
+ * such as default task priority.
+ *
+ * @author David Renteria
+ * @version 0.1.0
+ * @since 4/8/2026
+ */
+public interface SettingsDAO {
+}
