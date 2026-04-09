c4051348ef380d3b748a06cfdc730db19ab8b864
darenteria-cpu <darenteria@csumb.edu>
2026-04-08 19:24:17 -0700

    created SQLiteSettingsDAOTest.java

---
+++ src/test/java/com/tracker/SQLiteSettingsDAOTest.java
@@ -0,0 +1,13 @@
+package com.tracker;
+
+/**
+ * Represents the unit tests for SQLiteSettingsDAO.
+ * This class contains test cases to verify the correct functionality
+ * of the SQLiteSettingsDAO implementation,
+ *
+ * @author David Renteria
+ * @version 0.1.0
+ * @since 4/8/2026
+ */
+public class SQLiteSettingsDAOTest {
+}
