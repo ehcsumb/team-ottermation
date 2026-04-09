8216433fc9d7cbba8327db6ce5d417c3c3557b6c
darenteria-cpu <darenteria@csumb.edu>
2026-04-08 18:12:59 -0700

    created SQLiteTaskTypeDAOTest.java

---
+++ src/test/java/com/tracker/SQLiteTaskTypeDAOTest.java
@@ -0,0 +1,12 @@
+package com.tracker;
+
+/**
+ * Represents unit test for SQLiteTaskTypeDAO class,
+ * which handles all database operations related to task types.
+ *
+ * @author David Renteria
+ * @version 0.1.0
+ * @since 4/8/2026
+ */
+public class SQLiteTaskTypeDAOTest {
+}
