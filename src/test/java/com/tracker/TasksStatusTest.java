66af9441d098530a00b003f645310cf638d983b0
ltong-csumb <ltong@csumb.edu>
2026-04-14 04:26:22 -0700

    Set up TaskDAOTest to TaskStatusTest.

--- src/test/java/com/tracker/TasksStatusTest.java
+++ src/test/java/com/tracker/TasksStatusTest.java
@@ -1,7 +1,6 @@
-package com.tracker.dao;
+package com.tracker;
 
-import com.tracker.Database;
-import com.tracker.Task;
+import com.tracker.dao.TasksDAO;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,7 +10,7 @@ import java.sql.ResultSet;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class TasksDAOTest {
+class TasksStatusTest {
 
     @BeforeEach
     void setUp() {
