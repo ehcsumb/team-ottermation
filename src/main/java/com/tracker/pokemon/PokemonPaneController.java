e359a33fb6c12f2a53149e6f15c94bc33e5e6da6
Eric <eric@gtfo.org>
2026-04-21 06:27:12 -0700

    continuing

--- src/main/java/com/tracker/pokemon/PokemonPaneController.java
+++ src/main/java/com/tracker/pokemon/PokemonPaneController.java
@@ -1,8 +1,11 @@
 package com.tracker.pokemon;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import java.net.URI;
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
-import javax.swing.text.html.ImageView;
 
 
 /**
@@ -16,7 +19,12 @@ public class PokemonPaneController {
   @FXML private Pane pokePane;
   @FXML private ImageView pokeImage;
 
-
+  @FXML public void initialize() {
+    JsonNode apiRes = PokeAPI.getApiJson(URI.create("https://pokeapi.co/api/v2/pokemon/pikachu"));
+    String imageURI = apiRes.get("front_default").toString();
+    Image sprite = new Image(imageURI);
+    pokeImage.setImage(sprite);
+  }
 }
 
 
